cdb3ddce257fbdf1d12ddcb4ed1e8f477bef3de9
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-01-28 15:52:02 -0700

    added ability to split and create link and image nodes, added base tests

--- src/nodehelper.py
+++ src/nodehelper.py
@@ -44,3 +44,35 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     links_found = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return links_found
+
+def split_nodes_image(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        temp_nodes = []
+        temp_text = []
+        temp_images = []
+        temp_text = re.sub(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", '-', node.text)
+        temp_text = temp_text.split('-')
+        temp_images = extract_markdown_images(node.text)
+        for i in range(len(temp_text)-1):
+            temp_nodes.append(TextNode(temp_text[i], TextType.PLAINTEXT))
+            if i < len(temp_images):
+                temp_nodes.append(TextNode(temp_images[i][0], TextType.IMAGE, temp_images[i][1]))
+        nodes.extend(temp_nodes)
+    return nodes
+
+def split_nodes_link(old_nodes):
+    nodes = []
+    for node in old_nodes:
+        temp_nodes = []
+        temp_text = []
+        temp_links = []
+        temp_text = re.sub(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", '-', node.text)
+        temp_text = temp_text.split('-')
+        temp_links = extract_markdown_links(node.text)
+        for i in range(len(temp_text)-1):
+            temp_nodes.append(TextNode(temp_text[i], TextType.PLAINTEXT))
+            if i < len(temp_links):
+                temp_nodes.append(TextNode(temp_links[i][0], TextType.LINK, temp_links[i][1]))
+        nodes.extend(temp_nodes)
+    return nodes
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -169,6 +169,38 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
+    def test_split_images(self):
+        node = TextNode(
+                        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                        TextType.PLAINTEXT,
+                        )
+        new_nodes = nodehelper.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAINTEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAINTEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+            new_nodes,
+            )
+        
+    def test_split_links(self):
+            node = TextNode(
+                            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+                            TextType.PLAINTEXT,
+                            )
+            new_nodes = nodehelper.split_nodes_link([node])
+            self.assertListEqual(
+                [
+                    TextNode("This is text with a link ", TextType.PLAINTEXT),
+                    TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                    TextNode(" and ", TextType.PLAINTEXT),
+                    TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+                ],
+                new_nodes,
+                )
+
     
 
 if __name__ == "__main__":
