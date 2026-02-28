3b901d11b559112b5b1bbec914dc9293e2882b4b
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-02-28 13:13:33 -0700

    added markdown to block function that converts md to individual  blocks with base test

--- src/nodehelper.py
+++ src/nodehelper.py
@@ -95,4 +95,14 @@ def text_to_textnodes(text):
     nodes = split_nodes_link(nodes)
     return nodes
 
+def markdown_to_blocks(markdown):
+    block_to_return = []
+    md = markdown.split("\n\n")
+    for block in md:
+        block = block.strip()
+        if not block:
+            continue
+        else:
+            block_to_return.append(block)
+    return block_to_return
 
