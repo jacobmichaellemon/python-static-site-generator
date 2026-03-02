f4bb721dc3387ef47f6408ad12dd715b3eada809
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-03-01 20:36:33 -0700

    block to block type function and few base tests

--- src/markdownhelper.py
+++ src/markdownhelper.py
@@ -1,3 +1,34 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    paragraph = 1
+    heading = 2
+    code = 3
+    quote = 4
+    unordered_list = 5
+    ordered_list = 6
+
+def block_to_block_type(markdown):
+    heading = re.findall(r"(#{1,6} (.*))\w+", markdown)
+    code = re.findall(r"(`{3}\n(.*)\n`{3})+", markdown)
+    quote = re.findall(r"(> ?(.*))\w+", markdown)
+    ulist = re.findall(r"(- (.*))\w+", markdown)
+    olist = re.findall(r"1.(.*)", markdown)
+
+    if heading:
+        return BlockType.heading
+    if code:
+        return BlockType.code
+    if quote:
+        return BlockType.quote
+    if ulist:
+        return BlockType.unordered_list
+    if olist:
+        return BlockType.ordered_list
+    
+    return BlockType.paragraph
+
 def markdown_to_blocks(markdown):
     block_to_return = []
     md = markdown.split("\n\n")
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,7 @@
 import unittest
 
 import markdownhelper
+from markdownhelper import BlockType
 
 class TestHTMLNode(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -21,4 +22,60 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_block_to_blocktype_ulist(self):
+        md = """
+- This is a list
+- with items
+"""
+        block_type = markdownhelper.block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.unordered_list
+        )
+    def test_block_to_blocktype_olist(self):
+        md = """
+1. This is a list
+2. with items
+"""
+        block_type = markdownhelper.block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.ordered_list
+        )
+    def test_block_to_blocktype_ulist(self):
+        md = """
+```
+This is code
+```
+"""
+        block_type = markdownhelper.block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.code
+        )
+
+    def test_block_to_blocktype_quote(self):
+        md = """
+> quote about really cool things
+"""
+        block_type = markdownhelper.block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.quote
+        )
+
+    def test_block_to_blocktype_paragraph(self):
+        md = """
+ZZZZZZZZzzzzzzzZZZZZzZzzzzzzzzz
+zzzzz
+zzzz
+zz
+z
+"""
+        block_type = markdownhelper.block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.paragraph
         )
