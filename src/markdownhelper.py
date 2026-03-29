08d82a98317f192595ac3d51839454a54f95e31e
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-03-28 23:35:16 -0700

    markdown for paragraphs is working with a base test

--- src/markdownhelper.py
+++ src/markdownhelper.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from textnode import text_node_to_html_node
+from nodehelper import text_to_textnodes
+from htmlnode import ParentNode
 import re
 
 class BlockType(Enum):
@@ -39,3 +42,27 @@ def markdown_to_blocks(markdown):
         else:
             block_to_return.append(block)
     return block_to_return
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def markdown_to_html_node(markdown):
+    htmlnodes = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match(block_type):
+            case BlockType.paragraph:
+                lines = block.split("\n")
+                paragraph = " ".join(lines)
+                children = text_to_children(paragraph)
+                node = ParentNode("p", children)
+                htmlnodes.append(node)
+            case _:
+                return f"Issue creating html with {block}"
+    return ParentNode("div", htmlnodes)
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -79,3 +79,21 @@ z
             block_type,
             BlockType.paragraph
         )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdownhelper.markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
